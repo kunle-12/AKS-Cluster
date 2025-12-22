92a73836ebb75965170534b521e45a1b92ffebe1
Hammed Nureni <kunlex.hm@gmail.com>
2025-12-22 09:53:34 -0600

    add aks04

--- terraform-aks-platform/modules/key_vault/main.tf
+++ terraform-aks-platform/modules/key_vault/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_key_vault" "this" {
   tenant_id = data.azurerm_client_config.current.tenant_id
 
   sku_name                    = "standard"
-  enable_rbac_authorization   = true
+  rbac_authorization_enabled = true
   public_network_access_enabled = false
   purge_protection_enabled    = true
   soft_delete_retention_days  = 7
