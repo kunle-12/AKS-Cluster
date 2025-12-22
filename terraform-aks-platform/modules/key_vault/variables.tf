7f729bc8ebe2b0ee885981d267d76a2564018dc1
Hammed Nureni <kunlex.hm@gmail.com>
2025-12-22 10:15:22 -0600

    add aks08

--- terraform-aks-platform/modules/key_vault/variables.tf
+++ terraform-aks-platform/modules/key_vault/variables.tf
@@ -22,7 +22,7 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
-variable "tenant_id" {
-  description = "The Azure Active Directory tenant ID"
-  type        = string
-}
+# variable "tenant_id" {
+#   description = "The Azure Active Directory tenant ID"
+#   type        = string
+# }
